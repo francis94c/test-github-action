574771a2e3a9faa2e2c182082a288e3b16c53d58
Francis Ilechukwu <francis94c@gmail.com>
2020-01-24 11:12:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,4 +12,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+$sql = "SHOW DATABASES";
+$result = $conn->query($sql);
+if ($result === false) {
+    throw new Exception("Could not execute query: " . $conn->error);
+}
+
+$db_names = array();
+while($row = $result->fetch_array(MYSQLI_NUM)) { // for each row of the resultset
+    $db_names[] = $row[0]; // Add db name to $db_names array
+}
+
+echo "Database names: " . PHP_EOL . print_r($db_names, TRUE);
 ?> 
